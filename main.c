25c6653d7a1f32a156f3a9fbf5793e368f509662
LLoyderino <adrijan.bjedov@gmail.com>
2022-02-16 17:15:11 +0100

    added deletion

--- main.c
+++ main.c
@@ -53,14 +53,35 @@ void swap(int *array, int i, int j) {
 }
 
 // Turn our tree in a heap
-void heapify(MinHeap *root, int index) {
+void heapify_up(MinHeap *root, int index) {
   int key = root->data[index];
   int parent_index = (index - 1) / 2;
   int parent = root->data[parent_index];
 
   if (key < parent) {
     swap(root->data, index, parent_index);
-    heapify(root, parent_index);
+    heapify_up(root, parent_index);
+  }
+}
+
+void heapify_down(MinHeap *root, int index) {
+  int smallest = index;
+  int l = (index * 2) + 1;
+  int r = (index * 2) + 2;
+
+  int size = root->size - 1;
+
+  if(l < size && root->data[l] < root->data[smallest]) {
+    smallest = l;
+  }
+
+  if(r < size && root->data[r] < root->data[smallest]) {
+    smallest = r;
+  }
+
+  if(smallest != index) {
+    swap(root->data, index, smallest);
+    heapify_down(root, smallest);
   }
 }
 
@@ -71,7 +92,48 @@ void insert(MinHeap *root, int new_key) {
   root->data[root->size] = new_key;
   root->size++;
 
-  heapify(root, (root->size - 1));
+  heapify_up(root, (root->size - 1));
+}
+
+// Searches for a key in the heap
+// Returns the index, or -1 if nothing was found
+// todo: make it O(n/2) instead of O(n)
+int find(MinHeap *root, int key) {
+  int index = 0;
+
+  while(index < root->size && root->data[index] != key) {
+    index++;
+  }
+
+  if(index >= root->size) {
+    return -1;
+  }
+
+  return index;
+}
+
+
+// Delete an element from the heap
+void delete(MinHeap *root, int key) {
+  int index = find(root, key);
+  int last = root->size - 1;
+
+  swap(root->data, index, last);
+
+  // Delete the last element
+  root->data[last] = 0;
+  root->size--;
+
+  if(index == last)
+    return;
+
+  int parent = index / 2;
+
+  if(root->data[index] < root->data[parent]) {
+    heapify_up(root, index);
+  } else {
+    heapify_down(root, index);
+  }
 }
 
 // Print the array in a nice way
@@ -88,18 +150,32 @@ void print_array(int *data, int size) {
 int main() {
   MinHeap *h = new_heap();
   int input;
+  char cmd;
 
   if(h == NULL)
-    return 1;
+    return EXIT_FAILURE;
   
   do {
-    printf("Input: ");
-    scanf("%d", &input);
-    if(input > 0) {
-      insert(h, input);
-      print_array(h->data, h->size);
-    }
-  } while(input > 0);
+    printf("Command: ");
+    scanf("%c", &cmd);
 
-  return 0;
+    if(cmd == 'q')
+      break;
+
+    printf("Value: ");
+    scanf("%d", &input);
+
+    switch(cmd) {
+      case 'i':
+        insert(h, input);
+        break;
+      case 'd':
+        delete(h, input);
+        break;
+    }
+
+    print_array(h->data, h->size);
+  } while(cmd != 'q');
+
+  return EXIT_SUCCESS;
 }
